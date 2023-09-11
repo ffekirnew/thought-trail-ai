daa9333187b5a9035627a2ed440fafa589a729c3
ffekirnew <ffekirnew0808@gmail.com>
2023-09-11 20:53:33 +0300

    Define base response class

---
+++ backend/src/application/responses/base.response.ts
@@ -0,0 +1,23 @@
+class BaseResponse<T> {
+  public success: boolean;
+  public message: string;
+  public data: T;
+  public error: any;
+
+  constructor(success: boolean, message: string, data: any, error: any) {
+    this.success = success;
+    this.message = message;
+    this.data = data;
+    this.error = error;
+  }
+
+  static success<T>(message: string, data: T) {
+    return new BaseResponse<T>(true, message, data, null);
+  }
+
+  static error<T>(message: string, error: any) {
+    return new BaseResponse<T>(false, message, null as T, error);
+  } 
+}
+
+export default BaseResponse;
