92246ec431a9871d57c29f28b6b2fc3be2bae7e1
ffekirnew <ffekirnew0808@gmail.com>
2023-09-14 00:16:33 +0300

    Add react query to the mix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,12 +6,17 @@ import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import router from './routes'
 import theme from './theme'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
 )
