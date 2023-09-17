4260782264acc1976209d9cb7f7acfdd1e0d0975
ffekirnew <ffekirnew0808@gmail.com>
2023-09-17 21:21:16 +0300

    Added new section to landing page

---
+++ frontend/src/components/langing-page/HowToUse.tsx
@@ -0,0 +1,18 @@
+import { Flex, Heading, SimpleGrid, Image } from '@chakra-ui/react'
+import React from 'react'
+import FeatureCard from './FeatureCard'
+
+import addJournal from '../../assets/addJournal.png';
+import addCollection from '../../assets/addCollection.png';
+import chat from '../../assets/chat.png';
+
+const HowToUse = () => {
+  return <Flex flexDir={'column'} paddingX={{ base: 10, lg: 40 }} gap={10}>
+    <Heading textAlign={'left'}>How to use it?</Heading> 
+    <Image src={addJournal} width={'60%'} borderRadius={10} borderWidth={'1px'} />
+    <Image src={addCollection} width={'60%'} borderRadius={10} borderWidth={'1px'} />
+    <Image src={chat} width={'60%'} borderRadius={10} borderWidth={'1px'} />
+  </Flex>
+}
+
+export default HowToUse
