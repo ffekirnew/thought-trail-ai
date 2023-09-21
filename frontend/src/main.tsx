e88f2b1f5e8be966f1a6ed78c44116cc447036a6
ffekirnew <ffekirnew0808@gmail.com>
2023-09-21 10:08:04 +0300

    Add react query devtools for debugging

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import { RouterProvider } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { RouterProvider } from 'react-router-dom';
 
-import './index.css'
-import router from './routes'
-import theme from './theme'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import './index.css';
+import router from './routes';
+import theme from './theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient()
 
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
