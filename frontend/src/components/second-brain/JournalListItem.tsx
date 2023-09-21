2df0b88b640ee3260aafd82673ee1c13419f873b
ffekirnew <ffekirnew0808@gmail.com>
2023-09-21 20:24:27 +0300

    Improve journal listing

---
+++ frontend/src/components/second-brain/JournalListItem.tsx
@@ -0,0 +1,44 @@
+import { Button, HStack, Show, Spacer, Text, VStack, useColorMode } from "@chakra-ui/react";
+import { format } from "date-fns";
+import { Journal } from "../../services/journalsService";
+import { useNavigate } from "react-router-dom";
+
+const formatDate = (date: Date) => {
+  if (date) return format(new Date(date), 'MMM. dd, yyyy');
+  return format(new Date(), 'MMM. dd, yyyy');
+};
+
+interface Props {
+  journal: Journal;
+}
+
+const JournalListItem = ({ journal }: Props) => {
+  const { colorMode } = useColorMode();
+  const navigate = useNavigate();
+  const formattedDate = formatDate(journal?.createdAt!);
+
+  return (
+      <VStack
+        width={'100%'}
+        border={'1px'}
+        paddingX={5}
+        borderRadius={10}
+        borderColor={ colorMode === 'dark' ? 'gray.600' : 'gray.200' }
+        gap={5}>
+        <Button variant={'unstyled'} height={'auto'} textAlign={'left'} onClick={() => navigate('/everything/journals/' + journal?._id)} width={'100%'} gap={5}>
+          <VStack gap={2} paddingY={2} align={'left'}>
+            <HStack width={'100%'} gap={5}>
+              <Text fontWeight={'bold'} overflow={'hidden'}>{ journal?.title }</Text>
+              <Spacer />
+              <Text fontWeight={'light'} width={'6rem'}>{ formattedDate }</Text>
+            </HStack>
+            <Show above={'lg'}>
+              <Text fontWeight={'light'} overflow={'hidden'}>{ journal?.body }</Text>
+            </Show>
+          </VStack>
+        </Button>
+      </VStack>
+  );
+};
+
+export default JournalListItem;
---
+++ frontend/src/components/second-brain/skeletons/JournalListItemSkeleton.tsx
@@ -0,0 +1,11 @@
+import { Skeleton } from '@chakra-ui/react'
+import JournalListItem from '../JournalListItem'
+import { Journal } from '../../../services/journalsService'
+
+const JournalListItemSkeleton = () => {
+  return <Skeleton borderRadius={10}> 
+    <JournalListItem journal={{} as Journal} />
+  </Skeleton>
+}
+
+export default JournalListItemSkeleton
