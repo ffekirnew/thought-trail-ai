2517507ace5bb3e6f6737db1c90cc64ab7204495
ffekirnew <ffekirnew0808@gmail.com>
2023-09-21 20:24:06 +0300

    Add auth store for user state management

---
+++ frontend/src/state/useAuthStore.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand"
+import { User } from "../services/authClient";
+
+
+interface UseAuthStoreType {
+  user: User;
+  token: string;
+  setUser: (user: User) => void;
+  setToken: (token: string) => void;
+}
+
+const useAuthStore = create<UseAuthStoreType>(set => ({ 
+  user: {} as User,
+  token: "",
+  setUser: user => set({ user }),
+  setToken: token => set({ token })
+}))
+
+export default useAuthStore;
